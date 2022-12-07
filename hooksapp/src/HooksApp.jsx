538ce8841cb80d9b27468bd8f82379f9d13f0760
Marco Rivas <za19012397@zapopan.tecmm.edu.mx>
2022-12-06 18:46:01 -0600

    created a proyect of hooksapp and work in useState

---
+++ hooksapp/src/HooksApp.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Counter from "./useState/Counter";
+
+function HooksApp(){
+    return(
+        <div>
+            <h1>HooksApp</h1>
+            <Counter />
+        </div>
+    );
+}
+
+export default HooksApp;
