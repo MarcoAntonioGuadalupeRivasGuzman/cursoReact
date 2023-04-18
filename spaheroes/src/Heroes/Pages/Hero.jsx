126e898ed13205376faf4f52b2cb68993ebdffa0
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-17 20:13:49 -0600

    add new component for the hero and for the data prop

--- spaheroes/src/Heroes/Pages/Hero.jsx
+++ spaheroes/src/Heroes/Pages/Hero.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { GetHeroById, getHeroById } from "../Helpers/getHeroById";
 
 export const Hero=()=>{
+
+    const {id} = useParams();
+
+    const hero = getHeroById(id);
+
+    if(!hero){
+        return <Navigate to="/marvel" />
+    }
+
+    //console.log(hero)
+
     return(
         <>
-            <h1>Hero page</h1>
+            <h1>{hero.superhero}</h1>
+            <p>{hero.characters}</p>
+            <p>{hero.first_appearance}</p>
+            <p>{hero.publisher}</p>
         </>
     );
 }
