6e5018fb4b45a5850b6f5ab0c3f8251a951c736d
MarcoAntonioGuadalupeRivasGuzman <za19012397@zapopan.tecmm.edu.mx>
2023-04-17 20:15:44 -0600

    add new prop in the router :id for the hero page implements

--- spaheroes/src/Heroes/routes/HeroeRutes.jsx
+++ spaheroes/src/Heroes/routes/HeroeRutes.jsx
@@ -16,7 +16,7 @@ export const HeroeRoutes=()=>{
                     <Route path="marvel" element={<Marvel />}/>
                     <Route path="dc" element={<Dc/>}/>
                     <Route path="search" element={<Search/>}/>
-                    <Route path="hero" element={<Hero/>}/>
+                    <Route path="hero/:id" element={<Hero/>}/>
                     <Route path="/" element={<Navigate to="/marvel"/>}/>
                 </Routes>
             </div>
